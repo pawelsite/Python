94f475b6c2869fb09e1f0637884dc61866e3aef1
“pawel.site” <pawelb303@gmail.com>
2023-05-23 21:49:25 +0200

    serwer ready

--- tcpklient.py
+++ tcpklient.py
@@ -1,1 +1,11 @@
-print("It works!")
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MASSAGE = "!DISCONNECT!"
+SERVER = input("Please enter server IP address:\n")
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
