474b2863bde021b93198c93603288725d52aea36
“pawel.site” <pawelb303@gmail.com>
2023-05-23 22:30:06 +0200

    .

--- tcpklient.py
+++ tcpklient.py
@@ -5,9 +5,18 @@ PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MASSAGE = "!DISCONNECT!"
 SERVER = input("Please enter server IP address:\n")
+MASSAGE = input("Please enter your message:\n")
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
 
+send(MASSAGE)
